8cf5bdc2467aadb8507a98836eb983caa4a05c34
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-09-14 12:53:29 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -75,7 +75,7 @@ function geolocalizar(){
 			  cargarCategoria();
 		  };
 		  var errorFunction = function(){
-			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Carrión");
+			  alert("Compruebe que tiene activada la ubicación del dispositivo y que se encuentra cerca del municipio: Cazalla de la Sierra");
 			  loading(false);
 		  };
 		  loading(true);	
